8e403abebc571e4f37dc6bef012176b3eb2438f9
Amir Maghami <Maghami.A1987@gmail.com>
2025-11-18 19:00:34 +0330

    Create TheTabbar.vue

---
+++ src/layouts/PanelLayout/components/TheTabbar/TheTabbar.vue
@@ -0,0 +1,55 @@
+<script setup lang="ts">
+import TheButtonIcon from '@/components/TheButtonIcon.vue'
+import { Tooltip, Divider, Tabs, TabPane } from 'ant-design-vue/es'
+import { useTranslation } from 'i18next-vue'
+import { useTabStore } from '@/stores/tab.store'
+import { Icon } from '@iconify/vue'
+const isFullContent = defineModel<boolean>('isFullContent', { required: true })
+const tabStore = useTabStore()
+const { t } = useTranslation()
+</script>
+
+<template>
+  <section class="px-4 bg-gray-0 pt-2 flex items-center justify-between">
+    <Tabs
+      type="editable-card"
+      hide-add
+      v-model:activeKey="tabStore.activeTab"
+      @change="(key) => tabStore.setActiveTab(key as string)"
+      @edit="(key) => tabStore.removeTab(key as string)"
+      class="max-w-40 md:max-w-none md:flex-1"
+    >
+      <TabPane
+        v-for="tab in tabStore.computedTabs"
+        :key="tab.key"
+        :tab-key="tab.key"
+        :closable="tab.closable"
+      >
+        <template #tab>
+          <div class="flex items-center justify-center gap-2">
+            <Icon :icon="tab.icon" />
+            {{ tab.title }}
+          </div>
+        </template>
+      </TabPane>
+    </Tabs>
+    <div class="min-w-fit">
+      <Divider type="vertical" />
+      <Tooltip :title="isFullContent ? t('fullContentExit') : t('fullContent')" placement="bottom">
+        <TheButtonIcon
+          :icon="{
+            icon: isFullContent ? 'mdi:fullscreen-exit' : 'mdi:fullscreen',
+            class: 'text-primary text-base',
+          }"
+          @click="isFullContent = !isFullContent"
+        />
+      </Tooltip>
+    </div>
+  </section>
+</template>
+<style lang="less">
+.ant-tabs-dropdown-menu-title-content {
+  justify-content: space-between;
+  display: flex;
+}
+</style>
